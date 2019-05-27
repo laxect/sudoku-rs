65a064d0a7873abc8f9077aa52ace87ad7928b85
Gyara <me@gyara.moe>
2019-05-28 00:57:37 +0900

    board

--- src/board.rs
+++ src/board.rs
@@ -1,11 +1,47 @@
+use crate::error::*;
+
+#[derive(Default)]
 pub struct Board {
     inner: Vec<u8>,
 }
 
 impl Board {
-    fn new () -> Board {
+    pub fn new() -> Board {
         Board {
-            inner: vec![0; 81];
+            inner: vec![0; 81]
         }
+    }
+
+    pub fn unchecked_set(&mut self, x: usize, y: usize, val: u8) {
+        let pos = x*9 + y;
+        self.inner[pos] = val;
+    }
+
+    pub fn unchecked_get(&self, x: usize, y: usize) -> Option<u8> {
+        let pos = x*9 + y;
+        let val = self.inner[pos];
+        if val > 0 && val < 10 {
+            Some(val)
+        } else {
+            None
+        }
+    }
+
+    pub fn set(&mut self, x: usize, y: usize, val: u8) -> Result<(), SuDoKuError> {
+        if x >= 9 && y >= 9 {
+            return Err(SuDoKuError::OutOfBound);
+        }
+        if val > 9 || val < 1 {
+            return Err(SuDoKuError::InvalidValue);
+        }
+        self.unchecked_set(x, y, val);
+        Ok(())
+    }
+
+    pub fn get(&mut self, x: usize, y: usize) -> Result<Option<u8>, SuDoKuError> {
+        if x >= 9 && y >= 9 {
+            return Err(SuDoKuError::OutOfBound);
+        }
+        Ok(self.unchecked_get(x, y))
     }
 }
---
+++ src/error.rs
@@ -0,0 +1,16 @@
+use std::fmt;
+
+#[derive(Debug)]
+pub enum SuDoKuError {
+    InvalidValue,
+    OutOfBound,
+    NotSolveable,
+}
+
+impl fmt::Display for SuDoKuError {
+    fn fmt(&self,f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "{:?}", self)
+    }
+}
+
+impl std::error::Error for SuDoKuError {}
