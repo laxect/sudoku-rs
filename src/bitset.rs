97ca02860ab118e38f584d0d1ff210f41540fcc3
gyara <me@gyara.moe>
2019-05-28 15:24:01 +0800

    bit set test

--- src/bitset.rs
+++ src/bitset.rs
@@ -14,7 +14,7 @@ impl BitSet {
         if key >= 16 {
             return Err(SuDoKuError::OutOfBound);
         }
-        self.inner &= 1 << key;
+        self.inner |= 1 << key;
         Ok(())
     }
 
@@ -26,10 +26,48 @@ impl BitSet {
     }
 
     pub fn and(&mut self, another: &Self) {
-        self.inner &= another.inner;
+        self.inner |= another.inner;
     }
 
     pub fn count(&self) -> usize {
         self.inner.count_ones() as usize
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    #[test]
+    fn set() {
+        let mut bitset = BitSet::new();
+        bitset.set(1);
+        assert_eq!(bitset.inner, 2);
+    }
+    #[test]
+    fn set_get() {
+        let mut bitset = BitSet::new();
+        bitset.set(2).unwrap();
+        assert!(bitset.get(2).unwrap());
+    }
+    #[test]
+    fn and() {
+        let mut bitset_one = BitSet::new();
+        bitset_one.set(1);
+        let mut bitset_another = BitSet::new();
+        bitset_another.set(0);
+        bitset_one.and(&bitset_another);
+        assert!(bitset_one.get(0).unwrap());
+        assert!(bitset_one.get(1).unwrap());
+    }
+    #[test]
+    fn count() {
+        let mut bitset = BitSet::new();
+        let keys = vec![0, 1, 2, 1];
+        bitset.set(0);
+        bitset.set(1);
+        bitset.set(2);
+        bitset.set(1);
+        assert_eq!(bitset.inner, 7);
+        assert_eq!(bitset.count(), 3);
     }
 }
