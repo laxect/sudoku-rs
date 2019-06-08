29052bb9f83f2196805a9196fc598012ec287391
laxect <me@gyara.moe>
2019-06-08 22:34:54 +0900

    doc update

--- src/lib.rs
+++ src/lib.rs
@@ -4,7 +4,8 @@
 //! ```
 //! use sudoku_rs::{board, solver};
 //!
-//! let mut b = board::Board::new();
+//! let sudoku_str = "400000805030000000000700000020000060000080400000010000000603070500200000104000000";
+//! let mut b: board::Board = sudoku_str.parse().unwrap();
 //! let s = solver::DfsSolver::new();
 //! s.solve(&mut b);
 //! println!("{}", b);
